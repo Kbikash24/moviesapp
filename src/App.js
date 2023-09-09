e569691f74d02366e8d553112c08139e1c8617ca
Kbikash24 <bikashk11@gmail.com>
2023-09-09 22:42:25 +0530

    post

--- src/App.js
+++ src/App.js
@@ -1,106 +1,85 @@
-import React, { useState, useEffect, useMemo, useCallback } from "react";
-import MoviesList from "./components/MoviesList";
-import "./App.css";
-import "./components/loader.css";
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import AddMovie from './components/AddMovies';
+import MoviesList from './components/MoviesList';
+import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [retrying, setRetrying] = useState(false);
+  const [retry, setRetry] = useState(false);
  
-  const [formData, setFormData] = useState({
-    title: "",
-    openingText: "",
-    releaseDate: "",
-  });
-
-  // Memoize the MoviesList component to avoid re-rendering when other state changes
-  const memoizedMoviesList = useMemo(() => <MoviesList movies={movies} />, [movies]);
-
-  // Create a useCallback for fetchMoviesHandler to prevent unnecessary re-renders
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch('https://react-http-6b5e2-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
-        throw new Error("Something went wrong");
+        throw new Error('Something went wrong! retrying....');
       }
 
       const data = await response.json();
-      const transformMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
-      setRetrying(true);
-
-      // Retry after the specified delay
+      setRetry(true);
       setTimeout(() => {
-        setRetrying(false);
+        setRetry(false);
+        fetchMoviesHandler();
       }, 5000);
-    } finally {
-      setIsLoading(false);
     }
-  },[]);
+    setIsLoading(false);
+  }, []);
 
   useEffect(() => {
-    // Fetch data when the component mounts and on page reload
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function cancelRetryHandler() {
-    setRetrying(false);
+  const cancelRetry = () => {
+    setRetry(false);
+  };
+ 
+  async function addMovieHandler(movie) {
+    const response = await fetch('https://react-http-6b5e2-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  const moviesList = useMemo(() => {
+    return <MoviesList movies={movies} />;
+  }, [movies]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
 
-    // Validate the form data (you can add more validation logic here)
 
-    // Create a new movie object
-    const newMovie = {
-      id: movies.length + 1, // Generate a unique ID for the new movie
-      ...formData,
-    };
 
-    // Add the new movie to the movies array
-    setMovies((prevMovies) => [...prevMovies, newMovie]);
+  let content = <p>Found no movies.</p>;
 
-    // Clear the form data
-    setFormData({
-      title: "",
-      openingText: "",
-      releaseDate: "",
-    });
-  };
-
-  let content = isLoading ? <p className="loader"></p> : memoizedMoviesList;
+  if (movies.length > 0) {
+    content = moviesList;
+  }
 
   if (error) {
     content = (
       <div>
         <p>{error}</p>
-        {retrying ? (
+        {retry ? (
           <div>
-            Retrying...
-            <button onClick={cancelRetryHandler}>Cancel</button>
+            <button onClick={cancelRetry}>Cancel</button>
           </div>
         ) : (
           <div>
@@ -111,46 +90,19 @@ function App() {
     );
   }
 
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
   return (
     <React.Fragment>
+<section>
+      <AddMovie onAddMovie={addMovieHandler}/>
+      </section>
       <section>
-        <h1>Star Wars Movies</h1>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section className="form">
-        {/* Add a form to add movies */}
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="title">Title:</label>
-            <input
-              type="text"
-              id="title"
-              name="title"
-              value={formData.title}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="openingText">Opening Text:</label>
-            <textarea
-              id="openingText"
-              name="openingText"
-              value={formData.openingText}
-              onChange={handleInputChange}
-            ></textarea>
-          </div>
-          <div>
-            <label htmlFor="releaseDate">Release Date:</label>
-            <input
-              type="date"
-              id="releaseDate"
-              name="releaseDate"
-              value={formData.releaseDate}
-              onChange={handleInputChange}
-            />
-          </div>
-          <button type="submit">Add Movie</button>
-        </form>
-      </section>
+      
       <section>{content}</section>
     </React.Fragment>
   );
