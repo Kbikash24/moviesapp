9e0ca82c396cb905b8ad29b65e05b8d1276811cc
Kbikash24 <bikashk11@gmail.com>
2023-09-09 19:36:48 +0530

    update5

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import "./components/loader.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // Initially set to true
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [retrying, setRetrying] = useState(false);
-  const retryDelay = 5000; // Retry after 5 seconds
+ 
+  const [formData, setFormData] = useState({
+    title: "",
+    openingText: "",
+    releaseDate: "",
+  });
 
   // Memoize the MoviesList component to avoid re-rendering when other state changes
   const memoizedMoviesList = useMemo(() => <MoviesList movies={movies} />, [movies]);
@@ -41,11 +46,11 @@ function App() {
       // Retry after the specified delay
       setTimeout(() => {
         setRetrying(false);
-      }, retryDelay);
+      }, 5000);
     } finally {
       setIsLoading(false);
     }
-  }, [retryDelay]);
+  },[]);
 
   useEffect(() => {
     // Fetch data when the component mounts and on page reload
@@ -55,6 +60,36 @@ function App() {
   function cancelRetryHandler() {
     setRetrying(false);
   }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate the form data (you can add more validation logic here)
+
+    // Create a new movie object
+    const newMovie = {
+      id: movies.length + 1, // Generate a unique ID for the new movie
+      ...formData,
+    };
+
+    // Add the new movie to the movies array
+    setMovies((prevMovies) => [...prevMovies, newMovie]);
+
+    // Clear the form data
+    setFormData({
+      title: "",
+      openingText: "",
+      releaseDate: "",
+    });
+  };
 
   let content = isLoading ? <p className="loader"></p> : memoizedMoviesList;
 
@@ -80,6 +115,41 @@ function App() {
     <React.Fragment>
       <section>
         <h1>Star Wars Movies</h1>
+      </section>
+      <section className="form">
+        {/* Add a form to add movies */}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="openingText">Opening Text:</label>
+            <textarea
+              id="openingText"
+              name="openingText"
+              value={formData.openingText}
+              onChange={handleInputChange}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="releaseDate">Release Date:</label>
+            <input
+              type="date"
+              id="releaseDate"
+              name="releaseDate"
+              value={formData.releaseDate}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Add Movie</button>
+        </form>
       </section>
       <section>{content}</section>
     </React.Fragment>
